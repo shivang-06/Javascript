9c9009132cb5325b9360368c5df6e583fd0f0c60
Shivang Sharma <shivang.sharma15@outlook.com>
2020-02-04 22:45:12 +0530

    A global script for creating directories (Folders)
    In this I have created a global script that can work on Command Prompt/Terminal.
    This script can be used to create folders/Directories.
    This is done using npm and shebang syntax.

---
+++ makeDir/mkdir.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+//How to make a script global
+
+//1.npm init -y => Package
+//2.bin property in JSON
+//3. rename file
+//4. shebang syntax 
+//5. npm link
+
+var fs = require("fs");
+
+var folderName = process.argv[2]
+if(typeof folderName == true){
+    console.log("Please enter folderName")
+    return
+}
+if(fs.existsSync(folderName)){          //To check if folderName already exists
+    console.log(folderName + " already exists")
+    return
+}
+fs.mkdirSync(folderName); // function to make a directory
+
+//print the folder created
+console.log("Folder "+ folderName+ " has been created");    
+
