fae693737fbe01e131eaf08035de2f94b6dcb14d
Shivang Sharma <shivang.sharma15@outlook.com>
2020-03-22 21:50:22 +0530

    Add files via upload

---
+++ CLI_Weather_App/commands/help.js
@@ -0,0 +1,7 @@
+module.exports = function () {
+    console.log(`weather [command] <options>
+    today .................. Show weather for today
+    forecast ............... Show 5-day weather forecast
+    version ................ show package version
+    help ................... Show help menu for a command`)
+}
